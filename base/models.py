b7482ccedca8ab4243bd4cd54038c811000c6c80
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-07-16 16:40:30 +0400

    Create models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Usermodel(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.TextField(blank=True, null=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(
+        max_length=128,
+        validators=[MinLengthValidator(6)]
+    )
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+
+
