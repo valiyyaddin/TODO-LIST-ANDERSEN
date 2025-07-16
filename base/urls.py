795809269f8e577b653d530e215af2c7cd17354f
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-07-16 16:51:43 +0400

    Add CRUD and admin page

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import  TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete
 
 urlpatterns = [
-    path('',views.taskList,name = 'tasks'),
+
+    # Web views
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', TaskDelete.as_view(), name='task-delete'),
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import  CreateView,UpdateView,DeleteView
+from .models import  Task
+from django.urls import  reverse_lazy
 
-# Create your views here.
-def taskList(request):
-    return HttpResponse('Todo-list')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    paginate_by = 3
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+class TaskCreate(CreateView):
+
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+class TaskUpdate(UpdateView):
+
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
